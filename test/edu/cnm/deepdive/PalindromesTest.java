8aaf68a61d66ce8bfbf4c98d7b3ef5a4214f96d9
jdrake16 <stovetops111@gmail.com>
2020-06-09 18:22:32 -0600

    Finished testing Palindromes.

--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -6,9 +6,27 @@ import org.junit.jupiter.api.Test;
 
 class PalindromesTest {
 
+  static final String[] knownPalindromes = {
+      "radar",
+      "abba",
+      "x",
+      ""
+  };
+
+  static final String[] knownNonPalindromes = {
+      "sonar",
+      "abb"
+  };
+
+
+
   @Test
   void testRecursive() {
     assertTrue(Palindromes.testRecursive("radar"));
     assertFalse(Palindromes.testRecursive("sonar"));
+    assertTrue(Palindromes.testRecursive("abba"));
+    assertFalse(Palindromes.testRecursive("abb"));
+    assertTrue(Palindromes.testRecursive("x"));
+    assertTrue(Palindromes.testRecursive(""));
   }
 }
