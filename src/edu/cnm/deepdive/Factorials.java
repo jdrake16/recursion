7c904c82b7cce14d7b2ef64ab898eed521a17845
jdrake16 <stovetops111@gmail.com>
2020-06-09 20:57:50 -0600

    Refactored factorials to Biginteger.

--- src/edu/cnm/deepdive/Factorials.java
+++ src/edu/cnm/deepdive/Factorials.java
@@ -1,22 +1,24 @@
 package edu.cnm.deepdive;
 
+import java.math.BigInteger;
+
 public class Factorials {
 
   /**
    *
-   * @param n
+   * @param val
    * @return the computed value of n!
    * @throws IllegalArgumentException when n < 0.
    */
-  public static long computeRecursive(int n) throws IllegalArgumentException {
-    long result;
-    if (n < 0) {
+  public static BigInteger computeRecursive(int val) throws IllegalArgumentException {
+    BigInteger result;
+    if (val < 0) {
       throw new IllegalArgumentException();
     }
-    if (n == 0) {
-      result = 1;
+    if (val == 0) {
+      result = BigInteger.valueOf(val);
     } else {
-      result = n * computeRecursive(n - 1);
+      result = BigInteger.valueOf(val).multiply(computeRecursive(val - 1));
     }
     return result;
   }
